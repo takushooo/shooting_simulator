b03d6a70eb5861d09f30ab4f035bbd9434dcabb5
j-kawasaki <j-kawasaki@ist.osaka-u.ac.jp>
2019-02-27 13:38:11 +0900

    reset to a7540ca4704d4c6e796367c9ef7fd3c0a9578747

--- server/jikkou.sh
+++ server/jikkou.sh
@@ -3,7 +3,7 @@ echo 'Player 2: ud 2000, dd, 0'
 echo 'Player 3: ud 0, dd, 2000'
 echo 'Player 4: ud 2000, dd, 2000'
 
-python main.py &
-python main.py -ud 2000 &
-python main.py -dd 2000 &
-python main.py -ud 2000 -dd 2000 &
+python Client.py &
+python Client.py -ud 2000 &
+python Client.py -dd 2000 &
+python Client.py -ud 2000 -dd 2000 &
